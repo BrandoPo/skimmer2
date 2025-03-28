0211d67ae7dc68f269b1c76c33903a3eee7af796
AlexJVon <lunarvonderau@gmail.com>
2025-03-27 22:12:11 -0400

    updated account model and new context

--- skimmer2/Models/account.cs
+++ skimmer2/Models/account.cs
@@ -1,44 +1,47 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public class account
+namespace skimmer2.Models
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int id { get; set; }
+    public class account
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
 
-    [Required]
-    [StringLength(50)]
-    public string username { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string username { get; set; }
 
-    [Required]
-    [EmailAddress]
-    [StringLength(100)]
-    public string email { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string email { get; set; }
 
-    [Required]
-    [StringLength(100)]
-    public string password { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string password { get; set; }
 
-    [Required]
-    [StringLength(50)]
-    public string first_name { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string first_name { get; set; }
 
-    [Required]
-    [StringLength(50)]
-    public string last_name { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string last_name { get; set; }
 
-    [Required]
-    [StringLength(255)]
-    public string address { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string address { get; set; }
 
-    [Required]
-    [EnumDataType(typeof(role))]
-    public role Role { get; set; }
-}
+        [Required]
+        [EnumDataType(typeof(role))]
+        public role Role { get; set; }
+    }
 
-public enum role
-{
-    User,
-    Admin
+    public enum role
+    {
+        User,
+        Admin
+    }
 }
