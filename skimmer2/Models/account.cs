4e5ff81e47732b9f4e46b3a768c389f51f78f121
AlexJVon <lunarvonderau@gmail.com>
2025-03-27 21:56:26 -0400

    keep website up

--- skimmer2/Models/account.cs
+++ skimmer2/Models/account.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public class AccountC
+public class account
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
