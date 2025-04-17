ed8470c21f068e3cc0fac7b3de207d5ccf94fe0f
AlexJVon <lunarvonderau@gmail.com>
2025-04-17 14:20:14 -0400

    Moved signin under acc and added session

--- skimmer2/Program.cs
+++ skimmer2/Program.cs
@@ -37,9 +37,11 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseSession();
 app.UseRouting();
 
 app.UseAuthorization();
+
 
 app.MapControllerRoute(
     name: "default",
